39af35a659bf85eb04565620435674b6340635bf
Mark Santoro <mark_santoro@comcast.net>
2024-08-16 14:28:09 -0700

    add navigation from content view to details view

---
+++ FavoriteBook/DetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsView.swift
+//  FavoriteBook
+//
+//  Created by Mark Santoro on 8/16/24.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsView()
+}
