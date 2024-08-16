799ef9855b8a2ef01367660daf0c2c5e8e05ff1a
Mark Santoro <mark_santoro@comcast.net>
2024-08-16 09:25:47 -0700

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Mark Santoro on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
